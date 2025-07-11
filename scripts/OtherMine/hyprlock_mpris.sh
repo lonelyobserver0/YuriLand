63aeb95060fbfcc1bfcb8bda241cf4a28f514333
lonelyobserver <lcmulaz@gmail.com>
2025-07-11 16:46:00 +0200

    works

--- scripts/OtherMine/hyprlock_mpris.sh
+++ scripts/OtherMine/hyprlock_mpris.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-OUT_FILE="/tmp/hyprlock_mpris.txt"
-
-while true; do
+#while true; do
     artist=$(playerctl metadata artist 2>/dev/null)
     title=$(playerctl metadata title 2>/dev/null)
     status=$(playerctl status 2>/dev/null)
 
     if [[ -n "$title" && -n "$artist" ]]; then
         if [[ "$status" == "Playing" ]]; then
-            icon=""
-        elif [[ "$status" == "Paused" ]]; then
             icon=""
+        elif [[ "$status" == "Paused" ]]; then
+            icon=""
         else
             icon=""
         fi
 
-        echo "$icon  $artist - $title" > "$OUT_FILE"
+        echo "$icon  $artist - $title"
     else
-        echo "" > "$OUT_FILE"
+        echo ""
     fi
 
-    sleep 2
-done
+    #sleep 2
+#done
